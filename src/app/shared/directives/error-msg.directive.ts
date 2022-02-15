ebcdd8d53fd0b2530d0e11614d6f7f0b29286d56
Pablo A.S PC Laublo <pabloalvarezseguel@gmail.com>
2022-02-15 20:08:45 -0300

    seccion 23 clase 351

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -7,15 +7,21 @@ export class ErrorMsgDirective implements OnInit{
   
   htmlElement : ElementRef<HTMLElement>;
   @Input() color : string = 'blue';
+  @Input() mensaje : string = "Debe ingresar este campo";
 
   constructor( private element : ElementRef<HTMLElement>) {
     this.htmlElement = element;
   }
   ngOnInit(): void {
     this.setColor();
+    this.setMensaje();
   }
   setColor () :void{
     this.htmlElement.nativeElement.style.color = this.color;
+    this.htmlElement.nativeElement.classList.add('form-text')
+  }
+  setMensaje () :void{
+    this.htmlElement.nativeElement.innerText = this.mensaje;
   }
 
 }
